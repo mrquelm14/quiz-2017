93415d4971b59c31c627012156644654525fbcd3
mrquelm14 <meer.riquelme@gmail.com>
2017-06-07 01:16:10 +0200

    practica53

--- routes/index.js
+++ routes/index.js
@@ -140,5 +140,19 @@ router.get('/help', function(req, res, next) {
    res.render('help'); 
 });
 
+// Definición de rutas de juego
+router.get('/quizzes',                     		quizController.index);
+router.get('/quizzes/:quizId(\\d+)',       		quizController.show);
+router.get('/quizzes/new',                 		quizController.new);
+router.post('/quizzes',                    		quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  		quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       		quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    		quizController.destroy);
+
+router.get('/quizzes/:quizId(\\d+)/play',  		quizController.play);
+router.get('/quizzes/:quizId(\\d+)/check', 		quizController.check);
+
+router.get('/quizzes/randomplay',	   		quizController.randomplay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)',	quizController.randomcheck);
 
 module.exports = router;
